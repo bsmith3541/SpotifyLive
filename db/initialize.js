8ec951720a98b891519913657455e19620a64aff
Brandon Smith <anotherbrandonsmith@gmail.com>
2016-11-02 15:17:22 -0400

    Added support for table initialization. Users table is created on the condition that it doesn't already exist.

---
+++ db/initialize.js
@@ -0,0 +1,20 @@
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/spotify_live';
+
+module.exports.initializeDb = function() {
+  const client = new pg.Client(connectionString);
+  client.connect();
+  pg.connect(connectionString, (err, client) => {
+    if(err) {
+      done();
+      console.log(err);
+    }
+
+    const query = client.query(
+      'CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name text not null, email text not null)'
+    );
+    query.on('end', () => {
+      client.end();
+    });
+  });
+}
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 var config = require('./config');
 const port = process.env.PORT || 8888;
+var db = require('./db/initialize');
 
 /**
  * Generates a random string containing numbers and letters
@@ -58,51 +59,51 @@ app.get('/callback', function(req, res) {
         error: 'state_mismatch'
       }));
   } else {
-    res.clearCookie(stateKey);
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: config.redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(config.client_id + ':' + config.client_secret).toString('base64'))
-      },
-      json: true
-    };
+      res.clearCookie(stateKey);
+      var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+          code: code,
+          redirect_uri: config.redirect_uri,
+          grant_type: 'authorization_code'
+        },
+        headers: {
+          'Authorization': 'Basic ' + (new Buffer(config.client_id + ':' + config.client_secret).toString('base64'))
+        },
+        json: true
+      };
 
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
+      request.post(authOptions, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
 
-        var access_token = body.access_token,
-            refresh_token = body.refresh_token;
+          var access_token = body.access_token,
+              refresh_token = body.refresh_token;
 
-        var options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
+          var options = {
+            url: 'https://api.spotify.com/v1/me',
+            headers: { 'Authorization': 'Bearer ' + access_token },
+            json: true
+          };
 
-        // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-          console.log(body);
-        });
+          // use the access token to access the Spotify Web API
+          request.get(options, function(error, response, body) {
+            console.log(body);
+          });
 
-        // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
-      } else {
-        res.redirect('/#' +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
-      }
-    });
-  }
+          // we can also pass the token to the browser to make requests from there
+          res.redirect('/#' +
+            querystring.stringify({
+              access_token: access_token,
+              refresh_token: refresh_token
+            }));
+        } else {
+          res.redirect('/#' +
+            querystring.stringify({
+              error: 'invalid_token'
+            }));
+        }
+      });
+    }
 });
 
 app.get('/refresh_token', function(req, res) {
